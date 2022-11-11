28b33f1ccd1dce3e3e17d17c7b01febe5c1c29e0
sonmh11 <115914781+sonmh11@users.noreply.github.com>
2022-11-11 10:40:21 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -1,11 +1,28 @@
 const express=require('express')
 const fs=require('fs')
 const app=express()
-const bodyparser = require('body-parser')
+const bodyparser=require('body-parser')
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static('public'))
+//app.set('view engine',ejs)
 
 app.get('/', (request, response)=>{
-    response.sendFile("c:/VSCODE/js2/map.html")
+    response.sendFile("C:/VSCODE/js2/map.html")
+    //response.sendFile("C:/VSCODE/js2/c.html")
+})
+
+app.get('/map_click',(request, response)=>{
+    response.sendFile("C:/VSCODE/js2/map_click.html")
+})
+
+app.get('/gym_data',(request,response)=>{
+    response.sendFile("C:/VSCODE/js2/gym_data.json")
+})
+
+app.get('/star',(request,response)=>{
+    response.sendFile("C:/VSCODE/js2/star.html")
 })
 
 app.listen(52273,()=>{
