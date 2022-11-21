0d64d0db37564995dfdc115df6c34fc92caaa291
sonmh11 <115914781+sonmh11@users.noreply.github.com>
2022-11-21 23:36:03 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -8,17 +8,33 @@ app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static('public'))
 //app.set('view engine',ejs)
 
-app.get('/', (request, response)=>{
+app.get('/a', (request, response)=>{    //test
+    response.sendFile("C:/VSCODE/js2/a.html")
+})
+
+app.get('/home', (request, response)=>{
+    response.sendFile("C:/VSCODE/js2/home.html")
+    //response.sendFile("C:/VSCODE/js2/a.html")
+})
+
+app.get('/map', (request, response)=>{
     response.sendFile("C:/VSCODE/js2/map.html")
-    //response.sendFile("C:/VSCODE/js2/c.html")
 })
 
 app.get('/map_click',(request, response)=>{
     response.sendFile("C:/VSCODE/js2/map_click.html")
 })
 
+app.get('/map_click_css',(request, response)=>{
+    response.sendFile("C:/VSCODE/js2/map_click.css")
+})
+
 app.get('/gym_data',(request,response)=>{
     response.sendFile("C:/VSCODE/js2/gym_data.json")
+})
+
+app.get('/test_data',(request,response)=>{  //test
+    response.sendFile("C:/VSCODE/js2/test_data.json")
 })
 
 app.get('/star',(request,response)=>{
@@ -37,6 +53,10 @@ app.get('/star_png',(request,response)=>{
     response.sendFile("C:/VSCODE/js2/img/starrate.png")
 })
 
-app.listen(52273,()=>{
-    console.log('server Start')
+app.get('/home_png',(request,response)=>{
+    response.sendFile("C:/VSCODE/js2/img/home.png")
+})
+
+app.listen(8080,()=>{
+    console.log('server Start at 8080')
 })
